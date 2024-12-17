5ab9123f2b3821d97611dafb9f6321183c264695
SzymonT2006 <156914033+SzymonT2006@users.noreply.github.com>
2024-12-17 09:32:02 +0100

    Add files via upload

---
+++ rezerwacja.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rezerwacja</title>
+</head>
+<body>
+<?php
+if(isset($_POST["data"])&& isset($_POST["osoby"])&& isset($_POST["telefon"])&&isset($_POST["dane_osob"])&&isset($_POST["rezerwuj"])){
+
+
+    
+}
+
+
+?>
+
+
+</body>
+</html>
