fe03ee90af1cf2102950582306813bfea386df1f
SzymonT2006 <156914033+SzymonT2006@users.noreply.github.com>
2024-12-19 14:35:45 +0100

    Add files via upload

--- rezerwacja.php
+++ rezerwacja.php
@@ -7,9 +7,23 @@
 </head>
 <body>
 <?php
-if(isset($_POST["data"])&& isset($_POST["osoby"])&& isset($_POST["telefon"])&&isset($_POST["dane_osob"])&&isset($_POST["rezerwuj"])){
+$connect = mysqli_connect("localhost","root","","baza1");
+if(mysqli_connect_errno()){
+    echo "CONNECTION FAILED";
+}
 
 
+if(isset($_POST["data"])&& isset($_POST["osoby"])&& isset($_POST["telefon"])&&isset($_POST["dane_osob"])){
+$data = $_POST["data"];
+$osoby = $_POST["osoby"];
+$telefon = $_POST["telefon"];
+$sql = "INSERT INTO rezerwacje (`id`,`nr_stolika`,data_rez,`liczba_osob`,telefon)
+values (null,null,'$data',$osoby,'$telefon')";
+if(mysqli_query($connect,$sql)){
+echo "<h1>Dodano rezerwację do bazy</h1>";
+
+}
+mysqli_close($connect);
     
 }
 
